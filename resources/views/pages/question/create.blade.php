911d6ae8370907059a6469548831c5c734d8ada9
Muhammad Qadri <mqad21@gmail.com>
2022-02-08 06:34:00 +0700

    Perbaikan tambah soal

--- resources/views/pages/question/create.blade.php
+++ resources/views/pages/question/create.blade.php
@@ -130,7 +130,9 @@
         if (val === -1) {
             $("#options-container").children().last().remove()
         } else {
-            $("#options-container").append(htmlOption($("#options-container").children().length))
+            const index = $("#options-container").children().length()
+            $("#options-container").append(htmlOption(index))
+            CKEDITOR.inline('option' + index)
         }
     }
 
